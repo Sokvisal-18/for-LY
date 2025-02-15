36a7b3e5f203378b798c171be949fabd062757ea
Sokvisal-18 <zsal4764@gmail.com>
2025-02-15 12:02:12 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,29 @@
-const wrapper = document.querySelector('.wrapper');
-const question = document.querySelector('.question');
-const gif = document.querySelector('.gif');
-const yesBtn = document.querySelector('.yes-btn');
-const noBtn = document.querySelector('.no-btn');
+document.addEventListener("DOMContentLoaded", () => { // Ensure DOM is fully loaded
+    const wrapper = document.querySelector(".wrapper");
+    const question = document.querySelector(".question");
+    const gif = document.querySelector(".gif");
+    const yesBtn = document.querySelector(".yes-btn");
+    const noBtn = document.querySelector(".no-btn");
 
-yesBtn.addEventListener('click', ()=>{
-    question.innerHTML = "I love you too!"
-    gif.src = "https://www.google.com/search?q=cute+emoji+gif&rlz=1C1CHBD_enKH1094KH1094&oq=cute+&gs_lcrp=EgZjaHJvbWUqDggAEEUYJxg7GIAEGIoFMg4IABBFGCcYOxiABBiKBTIMCAEQABhDGIAEGIoFMgYIAhBFGEAyBggDEEUYOTIMCAQQABhDGIAEGIoFMgwIBRAAGEMYgAQYigUyDAgGEAAYQxiABBiKBTIGCAcQRRg80gEIMzcyMGowajeoAgCwAgA&sourceid=chrome&ie=UTF-8#vhid=jGuqXmj6dTA6_M&vssid=l"
+    yesBtn.addEventListener("click", () => {
+        question.innerHTML = "You are my girlfriend now!";
+        gif.src = "https://pic.funnygifsbox.com/uploads/2021/01/funnygifsbox.com-2021-01-31-08-40-50-91.gif";
+    });
+
+    noBtn.addEventListener("mouseover", () => {
+        const noBtnRect = noBtn.getBoundingClientRect();
+        const maxX = window.innerWidth - noBtnRect.width;
+        const maxY = window.innerHeight - noBtnRect.height;
+
+        const randomX = Math.floor(Math.random() * maxX);
+        const randomY = Math.floor(Math.random() * maxY);
+
+        noBtn.style.position = "absolute"; // Ensure noBtn can move freely
+        noBtn.style.left = randomX + "px";
+        noBtn.style.top = randomY + "px";
+    });
+
+    noBtn.addEventListener("click", () => {
+        question.innerHTML = "Why not? 😢";
+    });
 });
-noBtn.addEventListener('mouseover',()=>{
-    const noBtnRect = noBtn.getBoundingClientRect();
-    const maxX = window.innerWidth - noBtnRect.width;
-    const maxY = window.innerHeight = noBtnRect.height;
-
-    const randomX = Math.floor(Math.random() *maxX);
-    const randomY = Math.floor(Math.random() *maxY);
-
-    noBtn.style.left = randomX + "px";
-    noBtn.style.top = randomY + "px";
-})
